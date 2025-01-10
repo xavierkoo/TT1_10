4e49909f08f89d4772edad1f572358478c435853
Leong Kit Ye <sherylleongkitye@gmail.com>
2025-01-11 04:12:54 +0800

    fix: fixed date string in form component

--- client/components/landing-page/PopupDialog.js
+++ client/components/landing-page/PopupDialog.js
@@ -11,7 +11,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-export function DialogDemo({title, FormComponent}) {
+export default function PopupDialog({title, FormComponent}) {
   return (
     <Dialog>
       <DialogTrigger asChild>
