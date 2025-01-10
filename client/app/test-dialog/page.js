1ce9393d37da5241db9e7c014035858ba8bc17fe
Leong Kit Ye <sherylleongkitye@gmail.com>
2025-01-11 03:54:26 +0800

    feat: added PopupDialog component

---
+++ client/app/test-dialog/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+import AddRequestsForm from "@/components/landing-page/AddRequestsForm";
+import {Button} from '@/components/ui/button';
+
+export default function TestDialog() {
+    return (
+            <AddRequestsForm/>
+    )
+}
