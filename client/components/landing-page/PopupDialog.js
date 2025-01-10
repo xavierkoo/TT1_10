bec5aa29b29d183cd847dc1a56863bcd7e8e467f
Leong Kit Ye <sherylleongkitye@gmail.com>
2025-01-11 03:54:26 +0800

    feat: added PopupDialog component

---
+++ client/components/landing-page/PopupDialog.js
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function DialogDemo({title, FormComponent}) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">{title}</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>
+            
+          </DialogDescription>
+        </DialogHeader>
+        {FormComponent}
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+          <Button>Cancel</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
