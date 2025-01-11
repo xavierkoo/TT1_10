f9962ef4bdcee2f39906249f799ea6eef2db544b
leewW2k <killerrobbot@gmail.com>
2025-01-11 13:06:40 +0800

    add navigation bar

--- client/app/landing-page/page.js
+++ client/app/landing-page/page.js
@@ -4,14 +4,38 @@ import {Input} from '@/components/ui/input';
 import {Icons} from '@/components/icons';
 import LandingPageCard from "@/components/landing-page/LandingPageCard";
 import {Button} from '@/components/ui/button';
+import {useRouter} from "next/navigation";
+import { IoIosNotifications } from "react-icons/io";
+
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+} from "@/components/ui/navigation-menu"
 
 export default function LandingPage() {
+    const router = useRouter();
     return (
         <div>
+            <div className="flex justify-between items-center w-full p-5">
+                <>
+                    Carbon Credit Exchange
+                </>
+                <NavigationMenu className='flex items-end flex-end'>
+                    <NavigationMenuItem>
+                        <Button onClick={() => router.push('/request-page')}>
+                            <NavigationMenuLink>
+                                Outstanding Requests
+                            </NavigationMenuLink>
+                            <IoIosNotifications/>
+                        </Button>
+                    </NavigationMenuItem>
+                </NavigationMenu>
+            </div>
             <div className='h1'>Welcome, CompanysName</div>
             <div className='flex flex-row items-center justify-center gap-32'>
-                <LandingPageCard label="Carbon Credits" number="10000" />
-                <LandingPageCard label="Carbon Credits" number="9999" />
+                <LandingPageCard label="Carbon Credits" number="10000"/>
+                <LandingPageCard label="Carbon Credits" number="9999"/>
             </div>
             <div className='h2'>Outstanding Requests</div>
             <div>
