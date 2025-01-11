afb7e89907fdff22829c184d66c12b3ce75ff9c1
raphaelroshan <49832307+raphaelroshan@users.noreply.github.com>
2025-01-11 14:17:04 +0800

    update route whitespace

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -24,22 +24,22 @@ db.connect();
 
 //requestingCompany get from session, how to handle ID?
 routes.post("/updateRequest", (req, res) => {
-const { requestId, requestDate, companyName, requestingCompany, carbonPrice, carbonQuantity, requestingReason, requestType } = req.body;
+  const { requestId, requestDate, companyName, requestingCompany, carbonPrice, carbonQuantity, requestingReason, requestType } = req.body;
 
-const query = `
-    UPDATE outstandingRequests 
-    SET requestDate = ?, companyName = ?, requestingCompany = ?, carbonPrice = ?, carbonQuantity = ?, requestingReason = ?, requestType = ?
-    WHERE requestId = ?
-`;
-const values = [requestDate, companyName, requestingCompany, carbonPrice, carbonQuantity, requestingReason, requestType, requestId];
+  const query = `
+      UPDATE outstandingRequests 
+      SET requestDate = ?, companyName = ?, requestingCompany = ?, carbonPrice = ?, carbonQuantity = ?, requestingReason = ?, requestType = ?
+      WHERE requestId = ?
+  `;
+  const values = [requestDate, companyName, requestingCompany, carbonPrice, carbonQuantity, requestingReason, requestType, requestId];
 
-db.query(query, values, (error, results) => {
-    if (error) {
-        console.error("An error occurred while updating the request:", error);
-        res.status(500).send("An error occurred while updating the request");
-        return;
-    }
-    res.status(200).send("Request updated successfully");
-});
+  db.query(query, values, (error, results) => {
+      if (error) {
+          console.error("An error occurred while updating the request:", error);
+          res.status(500).send("An error occurred while updating the request");
+          return;
+      }
+      res.status(200).send("Request updated successfully");
+  });
 });
 module.exports = routes;
